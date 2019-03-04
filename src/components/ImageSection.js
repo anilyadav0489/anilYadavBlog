b0b76de9ef17cb26bd1c8a0b3989f73c88ee7b27
anilyadav0489 <anilyadav0489@gmail.com>
2019-03-05 01:44:05 +0530

    Added testimonials

--- src/components/ImageSection.js
+++ src/components/ImageSection.js
@@ -1,12 +1,39 @@
-import React, {Component} from 'react';
-import backgroundImage from '../resources/image-section-background.png'
+import React, { Component } from 'react';
+import backgroundImage1 from '../resources/image-section-background.png'
+import backgroundImage2 from '../resources/runningOnTheBeach.jpg'
 
 class ImageSection extends Component {
+    bgImages = [backgroundImage1, backgroundImage2];
+    state = { currentImage: this.bgImages[0], index: 0 };
+    reoccur = null;
+
+    componentDidMount() {
+        let index = -1;
+        this.reoccur = setInterval(() => {
+            index = index === this.bgImages.length - 1 ? 0 : index + 1;
+            this.keepChangingBackground(index);
+        }, 4000);
+    }
+
+    keepChangingBackground = (index) => {
+        if(index){
+            this.setState({ bgStyle1:'show', bgStyle2: 'hide', index });
+        } else {
+            this.setState({ bgStyle1:'hide', bgStyle2: 'show', index });
+        }
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.reoccur);
+    }
+
+
     render() {
         return (
             <div id="image-section" className="image-section">
                 <div className="image-container">
-                    <img src={backgroundImage} alt=""/>
+                    {this.state.index ? <img src={backgroundImage1} alt="" className={this.state.bgStyle1}/>: null}
+                    {this.state.index === 0 ? <img src={backgroundImage2} alt="" className={this.state.bgStyle2}/>: null}
                 </div>
                 <div className="my-name">
                     <span>A</span>
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,10 +1,39 @@
 import React, {Component} from 'react';
-
+import testimonialBackground from '../resources/testimonialBackground.jpg'
+import testimonyWriter1 from '../resources/Testimony-writer-1.jpg';
+import testimonyWriter2 from '../resources/Testimony-writer-2.jpg';
 class Testimonials extends Component {
+    testimonials = [{writer: testimonyWriter1, comments: 'He is too good', name: 'Himali'},
+                    {writer: testimonyWriter2, comments: 'lksjdfljsldfgood', name: 'Himali Jain'}];
+    state = {currentTestimony: this.testimonials[1]};
+    reoccur = null;
+
+    componentDidMount(){
+        let index = -1;
+        this.reoccur = setInterval(()=>{
+            index = index === this.testimonials.length - 1 ? 0 : index + 1;
+            this.keepChangingTestimony(index);
+        }, 2000);
+    }
+
+    keepChangingTestimony = (index)=>{
+        this.setState({currentTestimony: this.testimonials[index]});
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.reoccur);
+    }
     render() {
         return (
             <div id="testimonials-section" className="testimonials-section">
-                Testimonials
+                <img src={testimonialBackground} alt="" className="testimonial-background-image"/>
+                <div className="testimony-content-box">
+                    <div >
+                        <img src={this.state.currentTestimony.writer} className="testimony-writer-image" alt="writer" />
+                    </div>
+                    <div className="testimony-content">{this.state.currentTestimony.comments}</div>
+                    <div className="testimony-author-info">{this.state.currentTestimony.name}</div>
+                </div>
             </div>
         );
     }
