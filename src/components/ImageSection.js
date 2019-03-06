d6f871f82750fff58d5803d9be9b681948665afc
anilyadav0489 <anilyadav0489@gmail.com>
2019-03-07 02:15:45 +0530

    Gradual change in image section corrected

--- src/components/ImageSection.js
+++ src/components/ImageSection.js
@@ -3,37 +3,13 @@ import backgroundImage1 from '../resources/image-section-background.png'
 import backgroundImage2 from '../resources/runningOnTheBeach.jpg'
 
 class ImageSection extends Component {
-    bgImages = [backgroundImage1, backgroundImage2];
-    state = { currentImage: this.bgImages[0], index: 0 };
-    reoccur = null;
-
-    componentDidMount() {
-        let index = -1;
-        this.reoccur = setInterval(() => {
-            index = index === this.bgImages.length - 1 ? 0 : index + 1;
-            this.keepChangingBackground(index);
-        }, 4000);
-    }
-
-    keepChangingBackground = (index) => {
-        if(index){
-            this.setState({ bgStyle1:'show', bgStyle2: 'hide', index });
-        } else {
-            this.setState({ bgStyle1:'hide', bgStyle2: 'show', index });
-        }
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.reoccur);
-    }
-
-
+    
     render() {
         return (
             <div id="image-section" className="image-section">
                 <div className="image-container">
-                    {this.state.index ? <img src={backgroundImage1} alt="" className={this.state.bgStyle1}/>: null}
-                    {this.state.index === 0 ? <img src={backgroundImage2} alt="" className={this.state.bgStyle2}/>: null}
+                    <img src={backgroundImage2} alt="" className="fixed-image"/>
+                    <img src={backgroundImage1} alt="" className="changing-image"/>
                 </div>
                 <div className="my-name">
                     <span>A</span>
