1583409c0721f6991737c9f4a2ea0116a3d74a50
anilyadav0489 <anilyadav0489@gmail.com>
2020-07-21 23:34:47 +0530

    updated phone number

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,7 +8,7 @@ class Contact extends Component {
                     <div className="contact-heading"><span>Reach Me At</span></div>
                         <div className="contact-details">
                             <div>E-Mail: <a href="mailto:JustAnotherAnil@gmail.com">JustAnotherAnil@gmail.com</a></div>
-                            <div>Mobile: +91-84597-32047</div>
+                            <div>Mobile: +91-96431-36238</div>
                             <div>LinkedIn: <a href="https://www.linkedin.com/in/anil-yadav-53b67333/" target="_blank">https://www.linkedin.com/in/anil-yadav-53b67333/</a></div>
                             <div>GitHub: <a href="https://github.com/anilyadav0489" target="_blank">https://github.com/anilyadav0489</a></div>
                         </div>
